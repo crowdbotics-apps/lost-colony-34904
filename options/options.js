346bb367a86fc5583306ec955f6a689b523d2929
Crowdbotics <team@crowdbotics.com>
2022-04-24 21:39:00 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "lost_colony_34904",
+  url: "https://lost_colony_34904.botics.co",
+  api: "https://lost_colony_34904.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
